0fd32cfca59361d4b59f5870a572f3e2cc6c7e54
Sander Bouwhuis <sander@enrise.com>
2016-03-29 09:45:48 +0200

    Introduce no-confusing-arrow, promote some secondary es6 rules to error instead of warning

--- es6.js
+++ es6.js
@@ -3,25 +3,26 @@ module.exports = {
   extends: ['eslint-config-mm/es5'],
   parserOptions: {
     ecmaVersion: 6
-    // Can't use ES6 imports yet in node: https://github.com/nodejs/help/issues/53#issuecomment-165763446
-    // sourceType: 'module'
+  // Can't use ES6 imports yet in node: https://github.com/nodejs/help/issues/53#issuecomment-165763446
+  // sourceType: 'module'
   },
   env: {
     es6: true
   },
   rules: {
-    'arrow-parens': [1, 'always'],
-    'arrow-body-style': [1, 'always'],
-    'arrow-spacing': 1,
-    'no-const-assign': 1,
+    'arrow-parens': [2, 'always'],
+    'arrow-body-style': [2, 'always'],
+    'arrow-spacing': 2,
+    'no-const-assign': 2,
+    'no-confusing-arrow': 2,
     'no-var': 1,
     'object-shorthand': 1,
     'prefer-arrow-callback': 1,
-    'prefer-const': 1,
+    'prefer-const': 2,
     //  Can't use rest params yet: https://github.com/nodejs/node/issues/5411
     // 'prefer-rest-params': 1,
     'prefer-spread': 1,
     'prefer-template': 1,
-    'template-curly-spacing': [1, 'never']
+    'template-curly-spacing': [2, 'never']
   }
-}
+};
