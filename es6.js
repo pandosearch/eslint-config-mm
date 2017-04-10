713ea117f47ff2e70ac6f681db31be7072bfcae2
Sander Bouwhuis <sander@enrise.com>
2017-04-10 09:27:06 +0200

    Shorter notations

--- es6.js
+++ es6.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-  extends: ['eslint-config-mm/es5'],
+  extends: ['./es5.js'],
   parserOptions: {
     ecmaVersion: 6
   // Can't use ES6 imports yet in node: https://github.com/nodejs/help/issues/53#issuecomment-165763446
@@ -10,13 +10,13 @@ module.exports = {
     es6: true
   },
   rules: {
-    'arrow-parens': [2, 'always'],
-    'arrow-body-style': [2, 'always'],
+    'arrow-parens': [1, 'as-needed'],
+    'arrow-body-style': [1, 'as-needed'],
     'arrow-spacing': 2,
     'no-const-assign': 2,
     'no-confusing-arrow': 2,
     'no-var': 2,
-    'object-shorthand': [2, 'never'],
+    'object-shorthand': [1, 'always'],
     'prefer-arrow-callback': 2,
     'prefer-const': 2,
     //  Can't use rest params yet: https://github.com/nodejs/node/issues/5411
