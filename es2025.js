c6608c6136f39a2805dd9e4c02f9739be62c72b0
Roeland van Batenburg <roeland@pandosearch.com>
2026-06-15 20:00:35 +1200

    chore: add es2025 config and make it the default
    Add es2025.js extending es6.js with ecmaVersion 2025, enabling async/await,
    optional chaining, and other modern syntax. Update index.js to export the
    new config. es6.js is retained unchanged for projects that need ES6 parsing.
    Bump version to 2.1.0.

---
+++ es2025.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+  extends: ['./es6.js'],
+  parserOptions: {
+    ecmaVersion: 2025
+  }
+};
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 'use strict';
-module.exports = require('./es6');
+module.exports = require('./es2025');
